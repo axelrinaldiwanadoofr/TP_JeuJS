347aeab158160af0a6dbebe898b647bcb0417d82
RINALDI AXEL IDEAL DAVI <arinaldi7@0680034T.AC-STRASBOURG.FR>
2024-02-09 15:02:07 +0100

    Ajout de la classe AcMur contruite par héritage de Acteur

---
+++ acmur.js
@@ -0,0 +1,8 @@
+
+class AcMur extends Acteur
+{
+    constructor( x, y )
+    {
+        super( x, y, "images/mur.bmp") ;
+    }
+}
--- jeu.js
+++ jeu.js
@@ -4,3 +4,5 @@ var mur1 = new Acteur( 50, 50, "images/mur.bmp" ) ;
 var mur2 = new Acteur( 100, 50, "images/mur.bmp" ) ;
 
 mur2.setPosition( 100, 100 ) ;
+
+mur = new AcMur( 150, 100 ) ;
