704cc1cf6e80af61cf26aa10c5210292629fc106
RINALDI AXEL IDEAL DAVI <arinaldi7@0680034T.AC-STRASBOURG.FR>
2024-02-09 13:36:41 +0100

    Creation de la classe Acteur

---
+++ acteur.js
@@ -0,0 +1,23 @@
+
+
+class Acteur
+{
+    constructor( x, y, nomFichierImage )
+    {
+        this.x = x ; // Crée l'attribut x et on y copie la valeur contenue dans l'argument x
+        this.y = y ;
+
+        // Crée un élément HTML de type image
+        // que copie dans le nouvel attribut image
+        this.image = document.createElement( "img") ;
+
+        // Mise à jour des propiétés de l'image
+        this.image.src = nomFichierImage ;
+        this.image.style.position = "absolute" ;
+        this.image.style.left = x + "px" ;
+        this.image.style.top = y + "px" ;
+
+        // Insertion de l'image dans le contenu HTML de la page
+        document.body.appendChild( this.image ) ;
+    }
+}
--- jeu.js
+++ jeu.js
@@ -1,43 +1,4 @@
 
+var mur1 = new Acteur( 50, 50, "images/mur.bmp" ) ;
 
-
-// Cree un objet un objet vide
-
-var acteur1 = {} ;
-
-
-
-// Ajoute les proprietes x et y
-
-acteur1.x = 20 ;
-
-acteur1.y = 40 ;
-
-
-
-// Cree un objet du DOM du type IMG et stocke sa reference dans nouvel attribut
-
-// image que l'ajoute a l'objet
-
-acteur1.image = document.createElement( "img" ) ;
-
-
-
-// Initialise les proprietes de l'objet IMG 
-
-acteur1.image.src = "images/mur.bmp" ; // Associe un fichier contenant l'image
-
-acteur1.image.style.position = "absolute" ; // Pour que l'image puisse s'afficher n'importe ou
-
-acteur1.image.style.left = acteur1.x ; // Positionne le coin superieur gauche de l'image
-
-acteur1.image.style.top = acteur1.y ; // a partir des coordonnes de l'acteur
-
-
-
-// Insere l'image de l'acteur dans l'arboresence du DOM pour qu'elle 
-
-// soit affichee par le navigateur
-
-document.body.appendChild( acteur1.image ) ;
-
+var mur2 = new Acteur( 100, 50, "images/mur.bmp" ) ;
